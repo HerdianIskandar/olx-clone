832992d3256a01a65c4ff7e63ce72865509fddde
Herdian Iskandar <herdian.iskandar99@gmail.com>
2026-05-08 05:58:56 +0700

    Create insert_categories.sql
    menambah sql query insert categories

---
+++ insert_categories.sql
@@ -0,0 +1,24 @@
+-- Insert sample categories for OLX Clone
+-- This SQL script will populate the categories table with relevant categories
+
+INSERT INTO categories (name, icon) VALUES
+('Mobil', 'bi bi-car-front'),
+('Motor', 'bi bi-bicycle'),
+('Properti', 'bi bi-house'),
+('Elektronik', 'bi bi-phone'),
+('Komputer', 'bi bi-laptop'),
+('Fashion', 'bi bi-bag'),
+('Kesehatan & Kecantikan', 'bi bi-heart'),
+('Hobi & Olahraga', 'bi bi-trophy'),
+('Rumah Tangga', 'bi bi-house-door'),
+('Jasa', 'bi bi-briefcase'),
+('Kamera', 'bi bi-camera'),
+('Musik & Film', 'bi bi-music-note'),
+('Buku & Majalah', 'bi bi-book'),
+('Mainan & Anak', 'bi bi-balloon'),
+('Makanan & Minuman', 'bi bi-cup-straw'),
+('Pertanian', 'bi bi-tree'),
+('Hewan Peliharaan', 'bi bi-heart-pulse'),
+('Alat Berat', 'bi bi-gear'),
+('Perlengkapan Kantor', 'bi bi-building'),
+('Lain-lain', 'bi bi-three-dots');
